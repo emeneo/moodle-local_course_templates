7d5763a5ab8cac5bc973d633b28a88ad3aac18ec
Flotter Totte <flottertotte@emeneo.com>
2017-08-02 14:54:30 +0800

    version update

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
  */
 defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'local_course_templates';
-$plugin->version  = 2016033002;
+$plugin->version  = 2016033003;
 $plugin->requires = 2015041700;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = 'Course templates plugin Version 2016033001';
