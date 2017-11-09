8c58d50152db05ef42fef63619200a5e2cd3ba2c
Flotter Totte <flottertotte@emeneo.com>
2017-11-09 15:54:18 +0800

    en lang improvement / version update
    en lang improvement: improved error message
    version: 3.3-b

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
  */
 defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'local_course_templates';
-$plugin->version  = 2017091900;
+$plugin->version  = 2017110900;
 $plugin->requires = 2015041700;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = 'Course templates plugin Version 3.1-c';
+$plugin->release = 'Course templates plugin Version 3.3-b';
